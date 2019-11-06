2e4177b600c1a46b1f47538d48ace421318fc842
达芬奇的代码 <51364410+NOTLOOK@users.noreply.github.com>
2019-11-06 19:40:02 +0800

    Update wsgi.py

--- DjangoBlog/wsgi.py
+++ DjangoBlog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django博客项目.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBlog.settings')
 
 application = get_wsgi_application()
